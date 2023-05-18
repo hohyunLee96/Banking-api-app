38cd0ab62cd45517afdd85dd83304d974461c086
hohyunLee <hhhhhhlee@naver.com>
2023-05-18 14:51:40 +0200

    commit

--- banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
@@ -1,5 +1,7 @@
 package nl.inholland.bankingapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,16 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(generator = "account_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name="account_seq", initialValue = 1)
+    private int accountId;
+
+    @OneToMany
+    @JsonIgnoreProperties({"accounts"})
     private int userId;
+
     private String IBAN;
     private double balance;
     private double absoluteLimit;
