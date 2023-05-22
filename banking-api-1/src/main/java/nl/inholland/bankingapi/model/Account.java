5d8d1e7731d9d89e303272e0c1cf17bb2172fc2c
hohyunLee <hhhhhhlee@naver.com>
2023-05-22 19:47:10 +0200

    trying to fix the error

--- banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
@@ -1,23 +1,16 @@
 package nl.inholland.bankingapi.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
-
-import java.util.Objects;
 
 @Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Account {
 
     @Id
@@ -25,12 +18,69 @@ public class Account {
     @SequenceGenerator(name="account_seq", initialValue = 1)
     private Long accountId;
 
-    @ManyToOne
-    @JsonIgnoreProperties({"accounts"})
-    private Long userId;
+//    @ManyToOne
+//    @JsonIgnoreProperties({"accounts"})
+//    private Long userId;
 
     private String IBAN;
     private double balance;
     private double absoluteLimit;
     private AccountType accountType;
+
+    public Account(String iban, double balance, double absoluteLimit, String accountType) {
+        this.IBAN = iban;
+        this.balance = balance;
+        this.absoluteLimit = absoluteLimit;
+        this.accountType = AccountType.valueOf(accountType);
+    }
+    public Account() {
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+//    public Long getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Long userId) {
+//        this.userId = userId;
+//    }
+
+    public String getIBAN() {
+        return IBAN;
+    }
+
+    public void setIBAN(String IBAN) {
+        this.IBAN = IBAN;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAbsoluteLimit() {
+        return absoluteLimit;
+    }
+
+    public void setAbsoluteLimit(double absoluteLimit) {
+        this.absoluteLimit = absoluteLimit;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
 }
