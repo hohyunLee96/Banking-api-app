0ef51cbb85d68d4bd1ab72a7af199eb6a273f75c
hohyunLee <hhhhhhlee@naver.com>
2023-05-22 12:27:03 +0200

    d

--- banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/model/Account.java
@@ -5,10 +5,17 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 
 import java.util.Objects;
 
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
@@ -16,11 +23,11 @@ public class Account {
     @Id
     @GeneratedValue(generator = "account_seq", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name="account_seq", initialValue = 1)
-    private int accountId;
+    private Long accountId;
 
-    @OneToMany
+    @ManyToOne
     @JsonIgnoreProperties({"accounts"})
-    private int userId;
+    private Long userId;
 
     private String IBAN;
     private double balance;
