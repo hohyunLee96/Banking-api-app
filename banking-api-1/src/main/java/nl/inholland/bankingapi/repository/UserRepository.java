3d6206f6794f4076ab32af969f13990be80ffd3d
hohyunLee <hhhhhhlee@naver.com>
2023-05-28 16:33:51 +0200

    .

--- banking-api-1/src/main/java/nl/inholland/bankingapi/repository/UserRepository.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User getUserById(long id);
+    User findUserById(long id);
 }
