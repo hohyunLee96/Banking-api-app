96e18a1974a5995cc48d907391a2cd63df7522e3
AizazAhsan <aizazahsan23@gmail.com>
2023-06-02 17:13:20 +0200

    Cross origin added

--- banking-api-1/src/main/java/nl/inholland/bankingapi/controller/AuthenticationController.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/controller/AuthenticationController.java
@@ -6,16 +6,14 @@ import nl.inholland.bankingapi.model.dto.RegisterRequestDTO;
 import nl.inholland.bankingapi.service.AuthenticationService;
 import nl.inholland.bankingapi.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.AuthenticationException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -30,6 +28,11 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequestDTO) throws AuthenticationException {
         return ResponseEntity.ok(authenticationService.login(loginRequestDTO.email(), loginRequestDTO.password()));
+    }
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Front end and back end are connected!!";
     }
 
 }
