19687566e0b481803ac811ff6aa509bdd4d112db
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-06-03 21:28:31 +0200

    changed some things in backend for testing purposes

--- banking-api-1/src/main/java/nl/inholland/bankingapi/model/dto/TransactionGET_DTO.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/model/dto/TransactionGET_DTO.java
@@ -3,9 +3,7 @@ package nl.inholland.bankingapi.model.dto;
 
 import nl.inholland.bankingapi.model.TransactionType;
 
-import java.time.LocalDateTime;
-
 public record TransactionGET_DTO(long transactionId, String fromIban, String toIban, double amount, TransactionType type,
-                                 LocalDateTime timeStamp, long performingUserId) {
+                                 String timeStamp, long performingUserId) {
 
 }
