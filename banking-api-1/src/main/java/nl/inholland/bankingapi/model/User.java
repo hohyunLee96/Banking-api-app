4536c10d9cda9df73c0abbeb7d34b0498c56197e
domik <domi.kaniecki@gmail.com>
2023-06-04 22:59:59 +0200

    Commit

--- banking-api-1/src/main/java/nl/inholland/bankingapi/model/User.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/model/User.java
@@ -30,8 +30,7 @@ public class User {
     private String address;
     private String city;
     private String phoneNumber;
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<UserType> userType;
+    private UserType userType;
     private Boolean hasAccount;
     private Double dailyLimit;
     private Double transactionLimit;
@@ -40,7 +39,7 @@ public class User {
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Account> accounts = new ArrayList<>();
 
-   public User(String email, String password, String firstName, String lastName, String birthDate, String postalCode, String address, String city, String phoneNumber, List<UserType> userType, Double dailyLimit, Double transactionLimit, boolean hasAccount) {
+   public User(String email, String password, String firstName, String lastName, String birthDate, String postalCode, String address, String city, String phoneNumber, UserType userType, Double dailyLimit, Double transactionLimit, boolean hasAccount) {
         this.email = email;
         this.password = password;
         this.firstName = firstName;
