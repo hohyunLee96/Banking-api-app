a245eecc20500bcf4bd63b61a1ddf578fa9db234
AizazAhsan <aizazahsan23@gmail.com>
2023-06-10 14:37:54 +0200

    Committing before recreating keystore

--- banking-api-1/src/main/java/nl/inholland/bankingapi/jwt/JwtKeyProvider.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/jwt/JwtKeyProvider.java
@@ -1,11 +1,11 @@
 package nl.inholland.bankingapi.jwt;
+
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -13,27 +13,29 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
+import java.io.IOException;
+
+
 @Component
 public class JwtKeyProvider {
+
     @Value("${jwt.key-store}")
     private String keystore;
 
     @Value("${jwt.key-store-password}")
-    private String password;
+    private String keystorePassword;
 
     @Value("${jwt.key-alias}")
-    private String alias;
+    private String keyAlias;
 
     private Key privateKey;
 
     @PostConstruct
-    public void init() throws KeyStoreException, IOException, UnrecoverableKeyException, NoSuchAlgorithmException, CertificateException {
+    public void init() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
         Resource resource = new ClassPathResource(keystore);
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(resource.getInputStream(), password.toCharArray());
-        privateKey = keyStore.getKey(alias, password.toCharArray());
-
-        if (privateKey == null) throw new RuntimeException("Private key not found");
+        keyStore.load(resource.getInputStream(), keystorePassword.toCharArray());
+        privateKey = keyStore.getKey(keyAlias, keystorePassword.toCharArray());
     }
 
     public Key getPrivateKey() {
