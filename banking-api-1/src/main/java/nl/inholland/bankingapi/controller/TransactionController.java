234373b5aebdcebc81aacf5bf706184045910ac1
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-06-24 22:26:40 +0200

    some more tests and removing try catch from controller

--- banking-api-1/src/main/java/nl/inholland/bankingapi/controller/TransactionController.java
+++ banking-api-1/src/main/java/nl/inholland/bankingapi/controller/TransactionController.java
@@ -2,13 +2,11 @@ package nl.inholland.bankingapi.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.java.Log;
-import nl.inholland.bankingapi.exception.ApiRequestException;
 import nl.inholland.bankingapi.model.TransactionType;
 import nl.inholland.bankingapi.model.dto.TransactionDepositDTO;
 import nl.inholland.bankingapi.model.dto.TransactionPOST_DTO;
 import nl.inholland.bankingapi.model.dto.TransactionWithdrawDTO;
 import nl.inholland.bankingapi.service.TransactionService;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -44,65 +42,37 @@ public class TransactionController {
             @RequestParam(required = false) Long performingUser,
             @RequestParam(required = false) Date searchDate
     ) {
-        try {
-            return ResponseEntity.ok(transactionService.getAllTransactions(fromIban, toIban, fromDate, toDate, lessThanAmount, greaterThanAmount, equalToAmount, type, performingUser, searchDate));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.ok(transactionService.getAllTransactions(fromIban, toIban, fromDate, toDate, lessThanAmount, greaterThanAmount, equalToAmount, type, performingUser, searchDate));
     }
 
     @PostMapping
     @PreAuthorize("hasAnyRole('EMPLOYEE', 'CUSTOMER')")
     public ResponseEntity<Object> addTransaction(@RequestBody TransactionPOST_DTO transactionPOSTDto) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.addTransaction(transactionPOSTDto)));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        catch (Exception e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.addTransaction(transactionPOSTDto)));
     }
 
     @PostMapping("/withdraw")
     @PreAuthorize("hasAnyRole('EMPLOYEE', 'CUSTOMER')")
     public ResponseEntity<Object> withdraw(@RequestBody TransactionWithdrawDTO transactionWithdrawDTO) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.withdraw(transactionWithdrawDTO)));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.withdraw(transactionWithdrawDTO)));
     }
 
     @PostMapping("/deposit")
     @PreAuthorize("hasAnyRole('EMPLOYEE', 'CUSTOMER')")
     public ResponseEntity<Object> deposit(@RequestBody TransactionDepositDTO transactionDepositDTO) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.deposit(transactionDepositDTO)));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.convertTransactionResponseToDTO(transactionService.deposit(transactionDepositDTO)));
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<Object> getTransactionById(@PathVariable long id) {
-        try {
-            return ResponseEntity.ok(transactionService.getTransactionById(id));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
     }
 
-//get daily transactions left for logged in user
-    @GetMapping("/dailyTransactionsLeft" )
+    //get daily transactions left for logged in user
+    @GetMapping("/dailyTransactionsLeft")
     @PreAuthorize("hasAnyRole('EMPLOYEE', 'CUSTOMER')")
     public ResponseEntity<Object> getDailyTransactionsLeft(HttpServletRequest request) {
-        try {
-            return ResponseEntity.ok(transactionService.convertAmountLeftToDailyTransaction(request));
-        } catch (ApiRequestException e) {
-            throw new ApiRequestException(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.ok(transactionService.convertAmountLeftToDailyTransaction(request));
     }
-
 }
